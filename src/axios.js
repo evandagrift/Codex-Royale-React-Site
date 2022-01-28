770604fc10a980880bc4254d9ac5e7d87e670d63
evandagrift <evandagrift@gmail.com>
2022-01-28 02:30:48 -0800

    axios routing updated

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,10 @@
 import Axios from "axios";
-const corsFix = 'https://cors-anywhere.herokuapp.com/';
 const baseURL = "https://www.royaletracker.com/api/";
 const localURL = "//localhost:52003/api/"
 let headers = {};
 if (localStorage.user){
    headers.Authorization = `bearer ${localStorage.user['token']}`;
 }
-export const axios = Axios.create({baseURL:localURL,
+export const axios = Axios.create({baseURL:baseURL,
 headers,
 });
